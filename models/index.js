c9240d3a1c0e79b1e802d084502f36996fa06e2b
Sanket Mistry <sanket.mern.dev@gmail.com>
2025-06-29 14:07:42 +0530

    add dialect in db config

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require("sequelize");
 const dbConfig = require("../config/db");
+const pg = require('pg')
 
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
+    dialectModule: pg,
     logging: false,
     dialectOptions: {
         ssl: {
